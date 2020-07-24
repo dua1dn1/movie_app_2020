aa8b83150c4c3e20ef68cc2ca3673d8ef846de38
dua1dn1 <dua1dn1@naver.com>
2020-07-24 16:07:38 +0900

    state start

--- movie_app_2020_view/src/App.js
+++ movie_app_2020_view/src/App.js
@@ -1,40 +1,24 @@
 import React, { Component } from 'react';
 
+class App extends React.Component{
+  state = {
+    Count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
 
-function Food({name, picture}) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img stc = {picture}/>
-  </div>
-}
-const foodLike = [
-  {
-    name: "Kimchi"
-  },
-  {
-    name: "Samgyeopsal"
-  },
-  {
-    name: "Bibimbap"
-  },
-  {
-    name: "Doncasu"   
-  },
-  {
-    name: "Kimbap"
+   render(){
+    return <div>
+      <h1>The number is: {this.state.Count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>;
   }
-];
-
-
-function App() {
-  return (
-    <div>
-      {foodLike.map(dish => (
-      <Food name = {dish.name} picture = {dish.image} />
-      ))}
-    </div>
-  )
-
 }
+
 
 export default App;
